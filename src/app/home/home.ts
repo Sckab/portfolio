261747e1aa6392e3b91476c2a73647976470430e
Sckab <giulianodeamicis2009@gmail.com>
2026-01-15 21:33:11 +0100

    completed the home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core'
+import { Icon } from './icon/icon'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    Icon,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  readonly emailLabel = signal('Send An Email')
 
+  email: string = 'giuliano@scsdc-co.org'
+
+  handleEmailClick() {
+    navigator.clipboard.writeText(this.email)
+
+    this.emailLabel.set('Email Copied!')
+
+    setTimeout(() => {
+      this.emailLabel.set('Send An Email')
+    }, 1500)
+  }
 }
---
+++ src/app/home/icon/icon.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+  selector: 'pf-stack-icon',
+  imports: [],
+  templateUrl: './icon.html',
+  styleUrl: './icon.css',
+})
+export class Icon {
+  @Input() iconName!: string
+  @Input() tooltip!: string
+}
