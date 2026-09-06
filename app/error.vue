6c2712c26583db03cd3934b050f2c84956edd3af
Sckab <giulianodeamicis2009@gmail.com>
2026-09-06 21:47:56 +0200

    update the error pages

---
+++ app/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+import Error from "~/components/Error.vue";
+import Link from "~/components/Link.vue";
+
+const props = defineProps<{ error: NuxtError }>();
+</script>
+
+<template>
+  <div
+    class="bg-secondary h-dvh flex items-center justify-center flex-col gap-3"
+  >
+    <h1 class="text-primary text-9xl font-bold">{{ error.status }}</h1>
+    <Error class="text-4xl">{{ error.statusText }}</Error>
+    <Link link="/" text="Go back home" class="text-xl" />
+  </div>
+</template>
